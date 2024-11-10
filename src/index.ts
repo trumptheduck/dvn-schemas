ddd92ae1c5734e0d3f4a6a64f96a5066c08cc8b1
trumptheduck <nhatyt123@gmail.com>
2024-11-11 01:22:23 +0700

    update

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
-export {
+import { PermissionDocument, Permission, PermissionSchema } from "./auth/permission.schema";
+import { AccountType, User, UserDocument, UserSchema } from "./auth/user.schema";
 
+export {
+    AccountType,
+    User,
+    UserDocument,
+    UserSchema,
+    PermissionDocument,
+    Permission,
+    PermissionSchema,
 }
